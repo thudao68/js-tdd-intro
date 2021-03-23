747ace4b7a99d4b323ae22b69aba3620630c33d8
thudao68 <78425091+thudao68@users.noreply.github.com>
2021-03-23 08:20:54 +0100

    Ecriture des tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,16 @@
+// capitalizeFirstLetters.js
+const assert = require('assert');
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+// Check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+
+// Check that capitalizeFirstletters transforms 'i am learning TDD' correctly
+assert.strictEqual(capitalizeFirst('i am learning TDD'), 'I Am Learning TDD');
+
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirst('z'), 'Z');
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(''), '');
